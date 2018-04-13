b1ea1eb8f5778f17ed8d4f03546dc9782120aae1
Pam Pelkola <pelkola.pam@gmail.com>
2018-04-13 13:35:46 -0500

    created a simple calculator

---
+++ calculator.js
@@ -0,0 +1,29 @@
+var inputString = process.argv;
+
+// Parses the command line argument to capture the "operand" (add, subtract, multiply, etc) and the numbers
+var operand = inputString[2];
+var numOne = inputString[3];
+var numTwo = inputString[4];
+
+var outputNum;
+
+if(operand === 'add'){
+outputNum = parseFloat(numOne) + parseFloat(numTwo);
+} else if (operand === 'subtract') {
+outputNum = parseFloat(numOne) - parseFloat(numTwo);
+}else if(operand === 'multiply'){
+    outputNum = parseFloat(numOne) * parseFloat(numTwo);
+
+}else if(operand === 'divide'){
+    outputNum = parseFloat(numOne) / parseFloat(numTwo);
+}else if(operand === 'remainder'){
+    outputNum = parseFloat(numOne) % parseFloat(numTwo);
+}else if(operand === 'exp'){
+    outputNum = Math.pow(numOne, numTwo);
+}else if(operand === 'algebra'){
+outputNum = parseAlgebra(numOne);
+}else{
+    outputNum = "Not a recognized command"
+}
+
+console.log(outputNum);
